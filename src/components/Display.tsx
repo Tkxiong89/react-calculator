12f9838c8f3eb498427027141b23a0cd41bc0dbe
Tkxiong89 <txiong401@gmail.com>
2022-02-03 12:32:32 -0600

    added css to display component

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../App.css';
 
 function Display() {
 
@@ -6,7 +7,9 @@ function Display() {
 
   return (
     <div className='display'>
-        
+        <div className="first">5000</div>
+        <div className="math-op">+</div>
+        <div className="second">10000</div>
     </div>
   )
 }
