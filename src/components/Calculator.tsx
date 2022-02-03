8abc018cf73c99ab028f45b7b831fac230208d4a
Tkxiong89 <txiong401@gmail.com>
2022-02-03 12:18:11 -0600

    added css to calc component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Display from './Display';
+import '../App.css';
 
 export default class Calculator extends Component {
   render() {
-    return <div>
+    return <div className='calculator'>
         <Display />
     </div>
   }
