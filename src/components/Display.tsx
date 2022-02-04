044b4dd861ba91bda8886a1a10bb5200901abccb
Tkxiong89 <txiong401@gmail.com>
2022-02-03 19:40:16 -0600

    add math operation functionality and styling

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -4,19 +4,47 @@ import '../App.css';
 interface Props {
   numOne:string,
   numTwo:string,
-  mathOp: string
+  mathOp: string,
+  bool: boolean
 }
 
-function Display() {
-
+function Display({ numOne, numTwo, mathOp, bool }:Props) {
   
+  const mathCalculate = (one:string, two:string, operation:string):number => {
+    let firstNum:number = parseInt(one);
+    let secondNum:number = parseInt(two);
+    let sum:number = 0;
+    switch(operation) {
+      case '+':
+        sum = firstNum + secondNum;
+        break;
+      case '-': 
+        sum = firstNum - secondNum;
+        break;
+      case 'x': 
+      sum = firstNum * secondNum;
+        break;
+      case '/': 
+        sum = firstNum / secondNum;
+        break;
+    }
+    return sum;
+  }
 
   return (
+    
     <div className='display'>
-        <div className="first">5000</div>
-        <div className="operation">+</div>
-        <div className="second">10000</div>
-    </div>
+     { !bool ?
+     <>
+       <div className="first">{ numOne }</div>
+       <div className="operation">{ mathOp }</div>
+       <div className="second">{ numTwo }</div> 
+     </>
+      : <>
+          <div>{mathCalculate(numOne, numTwo, mathOp) }</div>  
+        </>
+     }
+    </div> 
   )
 }
 
--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,36 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component, createRef, useRef } from 'react';
 import Display from './Display';
 import '../App.css';
+
 export default class Numbers extends Component {
-    state = {
-        numOne: "",
-        numTwo: "",
-        mathOp: ""
+  state = {
+    numOne: "",
+    numTwo: "",
+    mathOp: "",
+    bool: false
+  }
+
+
+
+
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    if (!this.state.mathOp) {
+      this.setState({ numOne: e.currentTarget.textContent });
+    } else {
+      this.setState({ numTwo: e.currentTarget.textContent })
     }
-    
+
+  };
+
+  mathOpClicker = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    this.setState({ mathOp: e.currentTarget.textContent })
+  };
+
+  equalClick = (): void => {
+    this.setState({ bool: !this.state.bool })
+  };
+
+  clearDisplay = () => {
+    this.setState({
+      numOne: "",
+      numTwo: "",
+      mathOp: "",
+      bool: false
+    })
+  }
+
   render() {
     return <div className='numbers'>
-            <Display />
-            <div className="num-containers">
+      <Display numOne={this.state.numOne} numTwo={this.state.numTwo} mathOp={this.state.mathOp} bool={this.state.bool} />
+      <div className="num-containers">
 
-                <input type="button" className="num one" value={1}/>
-                <input type="button" className="num two" value={2}/>
-                <input type="button" className="num three" value={3}/>
-                <input type="button" className="num four" value={4}/>
-                <input type="button" className="num five" value={5}/>
-                <input type="button" className="num six" value={6}/>
-                <input type="button" className="num seven" value={7}/>
-                <input type="button" className="num eight" value={8}/>
-                <input type="button" className="num nine" value={9}/>
-                <input type="button" className="num zero" value={0}/>
+        <button className="num one" value={1} onClick={(e) => this.handleClick(e)}>1</button>
+        <button className="num two" value={2} onClick={(e) => this.handleClick(e)}>2</button>
+        <button className="num three" value={3} onClick={(e) => this.handleClick(e)}>3</button>
+        <button className="num four" value={4} onClick={(e) => this.handleClick(e)}>4</button>
+        <button className="num five" value={5} onClick={(e) => this.handleClick(e)}>5</button>
+        <button className="num six" value={6} onClick={(e) => this.handleClick(e)}>6</button>
+        <button className="num seven" value={7} onClick={(e) => this.handleClick(e)}>7</button>
+        <button className="num eight" value={8} onClick={(e) => this.handleClick(e)}>8</button>
+        <button className="num nine" value={9} onClick={(e) => this.handleClick(e)}>9</button>
+        <button className="num zero" value={0} onClick={(e) => this.handleClick(e)}>0</button>
 
-            </div>
-            <div className="math-op">
-                <input type="button" className='math plus' value={"+"}/>
-                <input type="button" className='math plus' value={"-"}/>
-                <input type="button" className='math plus' value={"x"}/>
-                <input type="button" className='math plus' value={"/"}/>
-            </div>
+      </div>
+      <div className="math-op">
+        <button className='math plus' value={"+"} onClick={(e) => this.mathOpClicker(e)}>+</button>
+        <button className='math plus' value={"-"} onClick={(e) => this.mathOpClicker(e)}>-</button>
+        <button className='math plus' value={"x"} onClick={(e) => this.mathOpClicker(e)}>x</button>
+        <button className='math plus' value={"/"} onClick={(e) => this.mathOpClicker(e)}>/</button>
+        <button className='math equal' value={"="} onClick={this.equalClick}>=</button>
+      </div>
+      <div>
+        <button className="math clear" onClick={this.clearDisplay}>Clear</button>
+      </div>
     </div>;
   }
 }
